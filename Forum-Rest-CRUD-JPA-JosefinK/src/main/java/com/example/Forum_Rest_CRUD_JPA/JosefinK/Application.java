01e27a73330f11de481378d19536147efe792834
josefinkl <josefin.klangefjall@gmail.com>
2025-03-11 10:20:24 +0100

    first commit

---
+++ Forum-Rest-CRUD-JPA-JosefinK/src/main/java/com/example/Forum_Rest_CRUD_JPA/JosefinK/Application.java
@@ -0,0 +1,13 @@
+package com.example.Forum_Rest_CRUD_JPA.JosefinK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ Forum-Rest-CRUD-JPA-JosefinK/src/test/java/com/example/Forum_Rest_CRUD_JPA/JosefinK/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Forum_Rest_CRUD_JPA.JosefinK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
