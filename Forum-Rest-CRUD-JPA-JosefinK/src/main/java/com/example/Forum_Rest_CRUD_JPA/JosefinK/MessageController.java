f05ee7a708ba45a2fec81edbc2f7589d16a71976
josefinkl <josefin.klangefjall@gmail.com>
2025-03-13 12:11:44 +0100

    MessageController added with put to update one message

--- Forum-Rest-CRUD-JPA-JosefinK/src/main/java/com/example/Forum_Rest_CRUD_JPA/JosefinK/MessageController.java
+++ Forum-Rest-CRUD-JPA-JosefinK/src/main/java/com/example/Forum_Rest_CRUD_JPA/JosefinK/MessageController.java
@@ -1,33 +1,33 @@
-//package com.example.Forum_Rest_CRUD_JPA.JosefinK;
-//
-//import jakarta.validation.Valid;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.PutMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//@RequestMapping("/messages")
-//public class MessageController {
-//    ChannelService channelService;
-//    MessageService messageService;
-//
-//    public MessageController(ChannelService channelService, MessageService messageService) {
-//        this.channelService = channelService;
-//        this.messageService = messageService;
-//    }
-//
-//    @PutMapping ("/message")
-//    public ResponseEntity<Message> updateMessageByID(@Valid @RequestBody Message newMessage) {
-//        Message M1 = messageService.updateMessage(newMessage);
-//
-//        if (M1 != null) {
-//            return ResponseEntity.accepted().body(newMessage);
-//        }else{
-//            return ResponseEntity.notFound().build();
-//        }
-//    }
-//
-//
-//}
+package com.example.Forum_Rest_CRUD_JPA.JosefinK;
+
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/messages")
+public class MessageController {
+    ChannelService channelService;
+    MessageService messageService;
+
+    public MessageController(ChannelService channelService, MessageService messageService) {
+        this.channelService = channelService;
+        this.messageService = messageService;
+    }
+
+    @PutMapping
+    public ResponseEntity<Message> updateMessageByID(@Valid @RequestBody Message newMessage) {
+        Message M1 = messageService.updateMessage(newMessage);
+
+        if (M1 != null) {
+            return ResponseEntity.accepted().body(newMessage);
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+
+}
