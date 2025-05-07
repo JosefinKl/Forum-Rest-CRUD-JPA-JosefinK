5dd321068d9ba1706a8bd9883a6d53bb54446d01
josefinkl <josefin.klangefjall@gmail.com>
2025-05-07 10:37:53 +0200

    2 unit tests for ChannelService

--- Forum-Rest-CRUD-JPA-JosefinK/src/test/java/com/example/Forum_Rest_CRUD_JPA/JosefinK/ChannelServiceTest.java
+++ Forum-Rest-CRUD-JPA-JosefinK/src/test/java/com/example/Forum_Rest_CRUD_JPA/JosefinK/ChannelServiceTest.java
@@ -2,8 +2,10 @@ package com.example.Forum_Rest_CRUD_JPA.JosefinK;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 
 //Unit test for methods in a service class
 class ChannelServiceTest {
@@ -12,20 +14,36 @@ class ChannelServiceTest {
     ChannelService channelService = new ChannelService(channelRepository);
 
     @Test
-    void getAllChannels() {
+    void getAllChannelsShouldReturnChannels() {
         //arrange
+        Channel c1 = new Channel("Test1", "Test 1 description");
+        Channel c2 = new Channel("Test2", "Test 2 description");
+        List<Channel> mockChannels = List.of(c1, c2);
 
+        when(channelRepository.findAll()).thenReturn(mockChannels);
 
         //act
-
+        List<Channel> channels = channelService.getAllChannels();
 
         //assert
+        assertNotNull(channels);
+        assertEquals(mockChannels.size(), channels.size());
+        assertEquals(mockChannels, channels);
     }
 
     @Test
-    void deleteChannels() {
+    void addChannelShouldCallSaveRepository() {
         //arrange
+        Channel channel = new Channel("Test","Test description");
+        when(channelRepository.save(channel)).thenReturn(channel);
+
         //act
+
+        Channel savedChannel = channelService.addChannel(channel);
+
         //assert
+        verify(channelRepository).save(channel);
+        assertNotNull(savedChannel);
+
     }
 }
